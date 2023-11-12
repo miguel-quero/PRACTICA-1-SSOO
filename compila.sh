d7efbf7e18fa291874810afbbe49f5450035f6f7
miguel <m.quero.2021@alumnos.urjc.es>
2023-11-12 13:22:01 +0100

    comentarios añadidos

--- compila.sh
+++ compila.sh
@@ -1,5 +1,10 @@
 #!bin/bash
 
+#Este mandato sirve para obtener el fichero .o de la libreria creada
 gcc -c libreria.c -o libreria.o
-ar rcs liblibreria.a libreria.o 
+
+#Este mandato crea la libreria liblibreria.a, indicando que se quiere intertar los ficheros objeto en la libreria, o reemplazarlos si ya están en ella.
+ar -rv liblibreria.a libreria.o 
+
+#Este mandato compila el código de test.c y se indica que se va a utilizar la libreria liblibreria.a
 gcc test.c -o test -L. liblibreria.a

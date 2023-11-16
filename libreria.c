dda586c451f2a7ab463caf86145e4135bead9215
AdriDM-urjc <a.duenas.2021@alumnos.urjc.es>
2023-11-16 20:42:46 +0100

    troubleshooting

--- libreria.c
+++ libreria.c
@@ -11,7 +11,7 @@ int head(int N)
     int lineasmostradas = 0;
 
     // este código coge la entrada que el usuario escribe y la muestra por pantalla. Se repite dependiendo del valor de lineas.
-    while (lineasmostradas < N && fgets(input, sizeof(input), stdin) != NULL) {
+    while (lineasmostradas < N && fgets(input, 1024, stdin) != NULL) {
         printf("%s", input);
         lineasmostradas++;
     }
@@ -27,19 +27,17 @@ int tail(int N)
     //creamos un puntero al array de strings que vamos a usar
     char **entrada = NULL;
     int numLineas = 0;
-    char *input = (char *)malloc(512*sizeof(char)); //variable para almacenar las lineas que el usuario introduce por teclado.
+    char *input = (char *)malloc(1024*sizeof(char)); //variable para almacenar las lineas que el usuario introduce por teclado.
 
-    while (fgets(input,sizeof(input),stdin) != NULL){
-        //quitamos el carácter de nueva linea
-        input[strcspn(input, "\n")] = '\0';
-        char *nuevaLinea = (char *)malloc(512*sizeof(char));
+    while (fgets(input,1024,stdin) != NULL){
+        char *nuevaLinea = malloc((strlen(input) + 1) * sizeof(char));
         //reservamos espacio para cada linea y la copiamos
         strcpy(nuevaLinea, input);
         //redimensionamos el array para que entre la nueva linea
         numLineas++;
         entrada = realloc(entrada, numLineas*sizeof(char *));
         //guardamos la linea
-        entrada[numLineas] = nuevaLinea;
+        entrada[numLineas-1] = nuevaLinea;
     }
 
     //pequeño calculo para leer las N ultimas lineas y restringir la lectura si hay menos lineas en la entrada que las pedidas
@@ -68,19 +66,23 @@ int longlines(int N)
     //creamos un puntero al array de strings que vamos a usar
     char **entrada = NULL;
     int numLineas = 0;
-    char *input = (char *)malloc(512*sizeof(char)); //variable para almacenar las lineas que el usuario introduce por teclado.
+    char *input = (char *)malloc(1024*sizeof(char)); //variable para almacenar las lineas que el usuario introduce por teclado.
 
-    while (fgets(input,sizeof(input),stdin) != NULL){
-        //quitamos el carácter de nueva linea
-        input[strcspn(input, "\n")] = '\0';
-        char *nuevaLinea = (char *)malloc(512*sizeof(char));
+    while (fgets(input,1024,stdin) != NULL){
+        char *nuevaLinea = malloc((strlen(input) + 1) * sizeof(char));
         //reservamos espacio para cada linea y la copiamos
         strcpy(nuevaLinea, input);
         //redimensionamos el array para que entre la nueva linea
         numLineas++;
         entrada = realloc(entrada, numLineas*sizeof(char *));
         //guardamos la linea
-        entrada[numLineas] = nuevaLinea;
+        entrada[numLineas-1] = nuevaLinea;
+    }
+
+    //array de indices para no mover los elementos del array
+    int indexArray [numLineas];
+    for(int i=0;i<numLineas;i++){
+        indexArray[i] = i;
     }
 
     //algoritmo de la burbuja
@@ -88,10 +90,10 @@ int longlines(int N)
     for(int i=0;i<numLineas-1;i++){
         cambio = false;
         for(int j=0;j<numLineas-i-1;j++){
-            if (sizeof(entrada[j]) < sizeof(entrada[j+1])){
-                input = entrada[j];
-                entrada[j] = entrada[j+1];
-                entrada[j+1] = input;
+            if (strlen(entrada[indexArray[j]]) < strlen(entrada[indexArray[j+1]])){
+                int temp = indexArray[j];
+                indexArray[j] = indexArray[j+1];
+                indexArray[j+1] = temp;
                 cambio=true;
             }            
         }
@@ -106,7 +108,7 @@ int longlines(int N)
 
     // imprimimos por pantalla las N ultimas lineas
     for(int i=0; i<N; i++){
-        printf("%s",entrada[i]);
+        printf("%s",entrada[indexArray[i]]);
     }
 
     // liberación de la memoria dinámica.

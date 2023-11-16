d1505e0e48c86b6843db2cee603f17bb3f656570
AdriDM-urjc <a.duenas.2021@alumnos.urjc.es>
2023-11-16 13:59:00 +0100

    longlines V1

--- libreria.c
+++ libreria.c
@@ -1,5 +1,6 @@
 #include "libreria.h"
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -39,16 +40,73 @@ int tail(int N)
         entrada[numLineas] = nuevaLinea;
     }
 
+    //pequeño calculo para leer las N ultimas lineas y restringir la lectura si hay menos lineas en la entrada que las pedidas
     int indice = numLineas - N;
     if (indice < 0){
         N = numLineas;
         indice = 0;
     }
 
-    // este código coge la entrada que el usuario escribe y la muestra por pantalla. Se repite dependiendo del valor de lineas.
+    // imprimimos por pantalla las N ultimas lineas
     for(int i=0; i<N; i++){
         printf("%s",entrada[indice + i]);
     }
+    
+    // liberación de la memoria dinámica.
+    for(int i=0;i<numLineas;i++){
+        free(entrada[i]);
+    }
+    free(input);
+    
+    return 0;
+}
+
+int longlines(int N)
+{
+    //creamos un puntero al array de strings que vamos a usar
+    char **entrada = NULL;
+    int numLineas = 0;
+    char *input = (char *)malloc(512*sizeof(char)); //variable para almacenar las lineas que el usuario introduce por teclado.
+
+    while (fgets(input,sizeof(input),stdin) != NULL){
+        //quitamos el carácter de nueva linea
+        input[strcspn(input, "\n")] = '\0';
+        char *nuevaLinea = (char *)malloc(512*sizeof(char));
+        //reservamos espacio para cada linea y la copiamos
+        strcpy(nuevaLinea, input);
+        //redimensionamos el array para que entre la nueva linea
+        numLineas++;
+        entrada = realloc(entrada, numLineas*sizeof(char *));
+        //guardamos la linea
+        entrada[numLineas] = nuevaLinea;
+    }
+
+    //algoritmo de la burbuja
+    bool cambio;
+    for(int i=0;i<numLineas-1;i++){
+        cambio = false;
+        for(int j=0;j<numLineas-i-1;j++){
+            if (sizeof(entrada[j]) < sizeof(entrada[j+1])){
+                input = entrada[j];
+                entrada[j] = entrada[j+1];
+                entrada[j+1] = input;
+                cambio=true;
+            }            
+        }
+        //optimizacion del algoritmo de la burbuja, si el bucle interno no cambia ningun elemento, se puede salir del bucle
+        if (cambio == false){break;}
+    }
+
+    //pequeño calculo para restringir la lectura si hay menos lineas en la entrada que las pedidas
+    if (numLineas < N){
+        N = numLineas;
+    }
+
+    // imprimimos por pantalla las N ultimas lineas
+    for(int i=0; i<N; i++){
+        printf("%s",entrada[i]);
+    }
+
     // liberación de la memoria dinámica.
     for(int i=0;i<numLineas;i++){
         free(entrada[i]);

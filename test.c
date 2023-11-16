e54221695b1c113266c742391822b4eb8bcec324
miguel-quero <m.quero.2021@alumnos.urjc.es>
2023-11-16 14:55:10 +0100

    test modificado

--- test.c
+++ test.c
@@ -1,18 +1,17 @@
 
 #include "libreria.h"
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
     int lineas; //variable para guardar el número de lineas elegido por el usuario.
-
-    if (argc == 1){ //si no se reciben argumentos entonces lineas es 10.
-        lineas = 10;
-    } else if (argc == 2) { //si se recibe un argumento n o -n se realiza el valor absoluto por si acaso.
-        lineas = abs(atoi(argv[1]));
-    } else{ //si se recibe más de 1 argumento se muestra este mensaje de error por pantalla.
-        printf("Escribe un numero N despues del mandato para ver las N primeras lineas o nada para ver las 10 primeras lineas");
-        return 1;
-    }
     
-    return head(lineas);
+    if (argc = 1){
+        lineas = 10;
+    } else if (argc = 2){
+        lineas = abs(atoi(argv[1]));
+    }
+ 
+   
+    return tail(lineas);
 }
